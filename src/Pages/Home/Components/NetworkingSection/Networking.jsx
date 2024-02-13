02e332071d09efd28036e7c69635d07a64ffe9cf
Nitish Raj <nitishrajpandey0707@gmail.com>
2024-02-14 00:03:04 +0530

    add some thing in network section

--- src/Pages/Home/Components/NetworkingSection/Networking.jsx
+++ src/Pages/Home/Components/NetworkingSection/Networking.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { featuresimg1 } from "../../../../assets";
+import { featuresimg1, featuresimg2 } from "../../../../assets";
 import { IoSparkles } from "react-icons/io5";
 
 function Networking() {
   return (
-    <div className="bg-ghostWhite">
+    <div className="bg-ghostWhite grid grid-cols-1 gap-12">
       <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-20 px-4 xs:px-8 ss:px-14 ">
         <div className="flex justify-center">
           <img src={featuresimg1} alt="" />
@@ -27,6 +27,30 @@ function Networking() {
             </p>
             <button className="Button">Join Us</button>
           </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-20 px-4 xs:px-8 ss:px-14 ">
+        <div>
+          <div className="flex items-center gap-4">
+            <IoSparkles className="text-[#0ABAB5] text-2xl" />{" "}
+            <h3 className="text-xl">BiWeekly Blog</h3>
+          </div>
+          <div className="py-5 font-Quicksand">
+            <h1 className="font-bold text-xl xxs:text-2xl ss:text-3xl pb-5">
+              CONNECT, READ, EXPLORE!
+            </h1>
+            <p className="pb-5">
+              Stay updated with the best of tips, resources, and stories written
+              by our community of students + alumni of Universities like
+              Harvard, UPenn, Northwestern, and more! Additionally, also learn
+              about career prospects and future lessons from alumni of IIM,
+              Venture capitalists, Founders, and CEOs.
+            </p>
+            <button className="Button">Join Us</button>
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <img src={featuresimg2} alt="" />
         </div>
       </div>
     </div>
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,5 @@
 import heroBanner from "./heroBanner.png"
 import featuresimg1 from "./featuresimg1.png"
+import featuresimg2 from "./featuresimg2.png"
 
-export { heroBanner, featuresimg1 }
+export { heroBanner, featuresimg1, featuresimg2 }
